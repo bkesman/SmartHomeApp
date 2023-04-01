56f12efd686baef5c43a88686a6b53e4bba1af77
bkesman <berkan.kesman@gmail.com>
2023-04-02 01:51:09 +0200

    Add images and app icon

---
+++ SmartHomeApp/UIImage+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+Extension.swift
+//  SmartHomeApp
+//
+//  Created by Berkan Kesman on 02/04/2023.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    static let deviceHeaterOffIcon = UIImage(named: "DeviceHeaterOffIcon")
+    static let deviceHeaterOnIcon = UIImage(named: "DeviceHeaterOnIcon")
+    static let deviceLightOffIcon = UIImage(named: "DeviceLightOffIcon")
+    static let deviceLightOnIcon = UIImage(named: "DeviceLightOnIcon")
+    static let deviceRollerShutterIcon = UIImage(named: "DeviceRollerShutterIcon")
+}
