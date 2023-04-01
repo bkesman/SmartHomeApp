725b8cc8da751e4c1cd85276f90a3b605001e219
bkesman <berkan.kesman@gmail.com>
2023-04-02 01:07:44 +0200

    Add window to scene

--- SmartHomeApp/SceneDelegate.swift
+++ SmartHomeApp/SceneDelegate.swift
@@ -10,15 +10,24 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    private var coordinator: RootCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let rootViewController: UIViewController = {
+            let rootViewController = UIViewController()
+            rootViewController.view = UIView()
+            rootViewController.view.backgroundColor = .white
+            return rootViewController
+        }()
+        coordinator = RootCoordinator(rootViewController: rootViewController)
+        coordinator?.start()
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = coordinator?.rootViewController
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
