950fdb2719d248cad83b7556f65827f88774d0c1
bkesman <berkan.kesman@gmail.com>
2023-04-02 15:07:50 +0200

    Add ListDevicesViewModel

--- SmartHomeApp/App/ListDevices/Model/LIstDevicesViewModel/ListDevicesViewModel.swift
+++ SmartHomeApp/App/ListDevices/Model/LIstDevicesViewModel/ListDevicesViewModel.swift
@@ -6,6 +6,16 @@
 //
 
 import Foundation
+import UIKit
 
 struct ListDevicesViewModel {
+    let devices: [DeviceViewModel]
+    static let empty = ListDevicesViewModel(devices: [])
+}
+
+struct DeviceViewModel {
+    let deviceName: String
+    let currentState: String
+    let currentStateIcon: UIImage?
+    let isWorking: Bool
 }
